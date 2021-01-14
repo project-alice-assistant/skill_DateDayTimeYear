af5058542de26d7ce28b3dbf431e018fdb3d4ae6
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-01-14 19:54:56 +0100

    better seconds

--- widgets/js/Clock.js
+++ widgets/js/Clock.js
@@ -3,18 +3,19 @@ class DateDayTimeYear_Clock {
 		this.uid = uid;
 		this.myDiv = document.querySelector(`[data-ref="DateDayTimeYear_clock_${this.uid}"]`);
 		this.clockUpdate();
+		this.up = true
 	}
 
 	clockUpdate() {
 		const today = new Date();
 		const h = String(today.getHours()).padStart(2, '0');
 		const m = String(today.getMinutes()).padStart(2, '0');
-		const s = today.getSeconds();
-		if (s === 0 || s % 2 === 0) {
+		if (this.up) {
 			this.myDiv.innerHTML = `${h}:${m}`;
 		} else {
 			this.myDiv.innerHTML = `${h} ${m}`;
 		}
+		this.up = !this.up
 		setTimeout(this.clockUpdate.bind(this), 500);
 	}
 }
